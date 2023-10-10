5b102ebd74ed026d8c19e23af678e5199544fac0
unknown <mywarhio@gmail.com>
2023-10-10 22:12:10 +0300

    added logger

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 import bot
+import logging
+
+logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w")
+
 while True:
     bot.start_bot()
+    logging.info('Error occurred: restarting bot')
